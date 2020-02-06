4a61292e76a1055723d27157dbcf2add7b58eb02
朱海文 <1032671220@qq.com>
2020-02-06 17:25:29 +0800

    yarn add nestjs-mongoose-crud

--- server/apps/admin/src/users/users.module.ts
+++ server/apps/admin/src/users/users.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
+import {InjectModel} from "nestjs-typegoose";
+import User from "@libs/db/modles/user.modle";
+import {Crud} from 'nestjs-mongoose-crud';
 
+@Crud({model:User})
 @Module({
   controllers: [UsersController]
 })
-export class UsersModule {}
+export class UsersModule {
+  constructor(@InjectModel(User) private readonly model){
+
+  }
+}
