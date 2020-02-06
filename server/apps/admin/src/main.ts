9de98e2bca1f4257427eefea7f9f705f21ad2cb7
朱海文 <1032671220@qq.com>
2020-02-06 17:42:09 +0800

    https://docs.nestjs.com/recipes/swagger

--- server/apps/admin/src/main.ts
+++ server/apps/admin/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // const 
+
+
+
+
   await app.listen(3000);
   console.log('http://localhost:3000');
 }
