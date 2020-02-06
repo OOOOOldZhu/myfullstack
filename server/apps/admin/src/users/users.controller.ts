a1b4446c4bed2b71f011ee74d16afd223a657300
朱海文 <1032671220@qq.com>
2020-02-06 17:40:17 +0800

    nill

--- server/apps/admin/src/users/users.controller.ts
+++ server/apps/admin/src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import {Controller} from '@nestjs/common';
+import {Crud} from "nestjs-mongoose-crud";
+import User from "@libs/db/modles/user.modle";
+import {InjectModel} from "nestjs-typegoose";
 
+@Crud({model: User})
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(@InjectModel(User) private readonly model) {
+
+    }
+}
--- server/apps/admin/src/users/users.module.ts
+++ server/apps/admin/src/users/users.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
-import {InjectModel} from "nestjs-typegoose";
-import User from "@libs/db/modles/user.modle";
-import {Crud} from 'nestjs-mongoose-crud';
 
-@Crud({model:User})
 @Module({
   controllers: [UsersController]
 })
 export class UsersModule {
-  constructor(@InjectModel(User) private readonly model){
 
-  }
 }
