9c3d875c0b9386de49001c1b8d0daf8ec925c05f
朱海文 <1032671220@qq.com>
2020-02-07 11:22:47 +0800

    users 接口文档的建立

--- server/apps/admin/src/users/users.controller.ts
+++ server/apps/admin/src/users/users.controller.ts
@@ -2,9 +2,10 @@ import {Controller} from '@nestjs/common';
 import {Crud} from "nestjs-mongoose-crud";
 import User from "@libs/db/modles/user.modle";
 import {InjectModel} from "nestjs-typegoose";
-
+import {ApiTags} from '@nestjs/swagger'
 @Crud({model: User})
 @Controller('users')
+@ApiTags('用户')
 export class UsersController {
     constructor(@InjectModel(User) private readonly model) {
 
--- server/libs/db/src/modles/user.modle.ts
+++ server/libs/db/src/modles/user.modle.ts
@@ -1,11 +1,20 @@
-import {prop} from '@typegoose/typegoose'
+import {prop,modelOptions} from '@typegoose/typegoose'
+import {ApiProperty} from '@nestjs/swagger'
 
-export default class User{
+@modelOptions({
+    schemaOptions:{
+        timestamps:true,
+        //toJSON:true
+    }
+})
+export default class User {
 
+    @ApiProperty({description: '用户名',example:'张三'})
     @prop()
-    username:string
+    username: string
 
+    @ApiProperty({description: '密码',example:'123456'})
     @prop()
-    password:string
+    password: string
 
 };
